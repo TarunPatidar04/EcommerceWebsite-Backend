8817e32c4106859c088281c8d9f58664c8e6385a
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-06-06 10:22:47 +0530

    create a mongodb setup

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("mongodb Connected Successfully!");
+  })
+  .catch((error) => {
+    console.log("mongodb connection error : ", error);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  console.log(`Server is running on port ${3000}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
