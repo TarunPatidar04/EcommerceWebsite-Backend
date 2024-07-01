f61ac18874ca7599316558b48c5a5d331082ab6b
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-07-01 13:23:08 +0530

    fix the error

--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -1,6 +1,6 @@
 import PaymentModel from "../models/PaymentModel.js";
 import Razorpay from "razorpay";
-import { dotenv } from "dotenv";
+import  dotenv  from "dotenv";
 dotenv.config();
 const razorpay = new Razorpay({
   key_id: process.env.key_id,
